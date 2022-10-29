155956b33d174a3bf3f1fe4ca08f7abedf0d5113
gena <genagreiner94@gmail.com>
2022-10-29 14:42:18 +0300

    feat: complete the task in folder number 01

---
+++ 01-read-file/index.js
@@ -0,0 +1,8 @@
+const { stdout } = process;
+const path = require('path');
+const fs = require('fs');
+
+fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err, data) => {
+  if (err) throw err;
+  stdout.write(data);
+});

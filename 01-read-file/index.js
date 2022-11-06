a15d18cc981d9d436645d3e1ce063b9e9e1353a4
gena <genagreiner94@gmail.com>
2022-11-06 19:56:22 +0200

    fix: change read file method to readStream

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -2,7 +2,13 @@ const { stdout } = process;
 const path = require('path');
 const fs = require('fs');
 
-fs.readFile(path.join(__dirname, 'text.txt'), 'utf-8', (err, data) => {
-  if (err) throw err;
+const readableStream = fs.createReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
+let data = '';
+
+readableStream.on('data', chunk => {
+  data += chunk;
+});
+
+readableStream.on('end', () => {
   stdout.write(data);
 });
